2489bc5df2da86d4cfa510d41b3fb6a533a4c8f8
Philipp Rehner <69816385+prehner@users.noreply.github.com>
2021-04-13 15:50:36 +0200

    fix constructor for HD3 (#8)

--- src/hd3.rs
+++ src/hd3.rs
@@ -2,7 +2,7 @@ use crate::dual::PyDual64;
 use num_hyperdual::*;
 use pyo3::prelude::*;
 
-#[pyclass(name="HD3_64")]
+#[pyclass(name = "HD3_64")]
 #[derive(Clone)]
 /// Hyper dual number using 64-bit-floats.
 pub struct PyHD3_64 {
@@ -24,12 +24,12 @@ impl From<PyHD3_64> for HD3_64 {
 #[pymethods]
 impl PyHD3_64 {
     #[new]
-    fn new(v: [f64; 4]) -> Self {
-        HD3::new(v).into()
+    fn new(eps: f64, v1: f64, v2: f64, v3: f64) -> Self {
+        HD3::new(eps, v1, v2, v3).into()
     }
 }
 
-#[pyclass(name="HD3Dual64")]
+#[pyclass(name = "HD3Dual64")]
 #[derive(Clone)]
 /// Hyper dual number using 64-bit-floats.
 pub struct PyHD3Dual64 {
@@ -52,6 +52,6 @@ impl From<PyHD3Dual64> for HD3Dual64 {
 impl PyHD3Dual64 {
     #[new]
     pub fn new(v0: PyDual64, v1: PyDual64, v2: PyDual64, v3: PyDual64) -> Self {
-        HD3::new([v0.into(), v1.into(), v2.into(), v3.into()]).into()
+        HD3::new(v0.into(), v1.into(), v2.into(), v3.into()).into()
     }
 }
